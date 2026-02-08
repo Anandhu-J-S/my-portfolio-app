369de4bc8e6067f0b6159e387be82cc33d53cfd1
Anandhu-J-S <anandhujs1920@gmail.com>
2026-02-08 09:49:39 +0530

    profile first push

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import "../Hero/Hero.css"
+import myResume from "../../../src/Resume/Resume.pdf"
 
 function Hero() {
     return (
@@ -8,8 +9,8 @@ function Hero() {
                     <div className="col-sm-6">
                         <h1 className="fw-bold text-light">Hi, I'm Anandhu</h1>
                         <h4 className="mb-3 text-light">Full Stack Developer | MERN & Spring Boot</h4>
-                        <button className="btn btn-light m-3">View Resume</button>
-                        <button className="btn btn-outline-light">Hire Me</button>
+                        <button className="btn btn-light m-3" onClick={() => window.open(myResume, '_blank')}>View Resume</button>
+                       
                     </div>
 
                     <div className="col-sm-6 d-flex  justify-content-end">
@@ -17,6 +18,8 @@ function Hero() {
                             <p className="text-light">Email : anandhujs1920@gmail.com</p>
 
                             <p className="text-light">Github : <a className="text-decoration-none text-light"   target="_blank" href="https://github.com/Anandhu-J-S">github profile</a></p>
+
+                            <p className="text-light">Linkedin : <a className="text-decoration-none text-light"   target="_blank" href="https://www.linkedin.com/in/anandhu-js-1673361a6/">linkedin profile</a></p>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/Skills/Skills.jsx
+++ frontend/src/components/Skills/Skills.jsx
@@ -1,36 +1,44 @@
-
-
+import java from "../../assets/images/java.png"
+import mongodb from "../../assets/images/mongodb.png"
+import mysql from "../../assets/images/mysql.png"
+import node from "../../assets/images/nodejs.png"
+import react from "../../assets/images/reactlogo.png"
+import springboot from "../../assets/images/springboot.png"
+import "../Skills/Skills.css"
 function Skills() {
 
     const skills = [
-        { name: "React", level: 88 },
-        { name: "Java", level: 85 },
-        { name: "Node.js", level: 80 },
-        { name: "Spring Boot", level: 80 },
-        { name: "MySQL", level: 75 },
-        { name: "MongoDB", level: 70 }
+        { name: "React", level: 88, img:  react  },
+        { name: "Java", level: 85, img:  java  },
+        { name: "Node.js", level: 80, img:  node  },
+        { name: "Spring Boot", level: 80, img:  springboot  },
+        { name: "MySQL", level: 75, img:  mysql  },
+        { name: "MongoDB", level: 70, img:  mongodb  }
     ]
 
 
 
     return (
-       <div className="container mb-5">
-         <div className="card shadow-lg p-3">
-            <h3>Skills</h3>
-            <div className="row">
-                {skills.map((skill, index) => (
-                    <div className="col-sm-4 mb-4" key={index}>
-                        <div className="card shadow-sm">
-                            <div className="card-body">
-                                <h5 className="card-title">{skill.name}</h5>
-                                <p>{`${skill.level}%`}</p>
+        <div className="container mb-5">
+            <div className="card shadow-lg p-3">
+                <h3>Skills</h3>
+                <div className="row">
+                    {skills.map((skill, index) => (
+                        <div className="col-sm-4 mb-4" key={index}>
+                            <div className="card shadow-sm">
+                                <div className="card-body">
+                                    <div className="d-flex">
+                                        <img className="img-size me-3" src={skill.img} alt="undefined img" />
+                                        <h5 className="card-title mt-3">{skill.name}</h5>
+                                    </div>
+                                    <p className="text-center mt-3">{`${skill.level}%`}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
-       </div>
     )
 }
 
